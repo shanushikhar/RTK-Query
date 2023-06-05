3e2638a782e8f4e8aa7f0cbb630bfc41efc42841
Shanu Shikhar <thisisshanushikhar@gmail.com>
2023-06-05 16:35:45 +0530

    Added authentication with jwt & cookies

--- Mern_Auth/Backend/controller/userController.js
+++ Mern_Auth/Backend/controller/userController.js
@@ -6,7 +6,21 @@ import generateToken from "../utils/createToken.js";
 // @route   POST /api/users/auth
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-  res.send("auth user");
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+
+    res.status(200).json({
+      _id: user._id,
+      email: user.email,
+      name: user.name,
+    });
+  } else {
+    res.status(400);
+    throw new Error("User not found..x");
+  }
 });
 
 // @desc    Register a new user
@@ -44,20 +58,61 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users/logout
 // @access  Public
 const logoutUser = (req, res) => {
-  res.send("logout user");
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "Logged out successfully" });
 };
 
 // @desc    Get user profile
 // @route   GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send("get profile");
+  console.log(req);
+  if (req.user) {
+    res.json({
+      _id: req.user._id,
+      name: req.user.name,
+      email: req.user.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 // @desc    Update user profile
 // @route   PUT /api/users/profile
 // @access  Private
 const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findOne(req.user._id);
+
+  if (user) {
+    (user.name = req.body.name || user.name),
+      (user.email = req.body.email || user.email);
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const doesEmailExist = await User.findOne({ email: user.email });
+    if (doesEmailExist) {
+      res.status(400);
+      throw new Error("Email already exists so cant change that..");
+    }
+
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
   res.send("update profile");
 });
 
--- Mern_Auth/Backend/models/userModel.js
+++ Mern_Auth/Backend/models/userModel.js
@@ -28,8 +28,12 @@ UserSchema.pre("save", async function (next) {
   }
 
   const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
+  this.password = bcrypt.hash(this.password, salt);
 });
+
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model("User", UserSchema);
 
